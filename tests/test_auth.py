85ddcf0113175ef9072ce4dd5d5d7e3ad61de925
Michal <m.podolak14@gmail.com>
2025-02-26 11:48:39 +0000

    test: change test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,33 @@
 from playwright.sync_api import Page, expect
+from models.user import UserModel
+import pytest
+from extension import db
+
+# create_user fixture -- | Michal |
+@pytest.fixture
+def create_user(app):
+    """Fixture to add a user to the database."""
+    with app.app_context():
+        # Create a user instance
+        test_user = UserModel(
+        id=None,
+        username='test_user',
+        password="Test12345!", 
+        email="test@email.com",
+        location="test_location"
+        )
+        # check if user exists in the database
+        existing_user = UserModel.query.filter_by(username=test_user.username).first()
+
+        # if user doesnt exist
+        if existing_user is None:
+            # add user to the database
+            db.session.add(test_user)
+            db.session.commit()  # Commit the changes to the database
+        else:
+            test_user = existing_user
+
+        return test_user
 
 # Test login function with correct data using Playwright -- | Michal |
 def test_correct_login_details_redirects_to_dashboard_in_browser(create_user, page):   
