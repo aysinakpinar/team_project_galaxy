13054734bb45a9ee3bad93ad7994f8ff473205eb
Amelia Terry <ameliaterry@live.com>
2025-02-27 15:20:31 +0000

    Form result no users found

--- blueprints/friends.py
+++ blueprints/friends.py
@@ -12,16 +12,32 @@ friends = Blueprint("friends", __name__, url_prefix="/friends")
 @friends.route('/find', methods=['GET', 'POST'])
 def find():
     form = FindFriendsForm()
+    users = []
 
-    friends = UserModel.query.filter(
-    (UserModel.location == form.location.data) &
-    (UserModel.username != form.username.data)
-    ).all()
+    if form.validate_on_submit():
+        location = form.location.data
+        users = UserModel.query.filter(UserModel.location == location).all()
+
+        if not users:
+            return "No friends found"
+            # return render_template("friends.html", form=form, users=users)
+        
+        return render_template("friends.html", form=form, users=users)
     
-    if not friends:
-        return "No friends found"
+    return render_template("friends.html", form=form, users=users)
     
-    return render_template("friends.html", form=form)
+    # else:
+    #     return "No friends found"
+
+    # friends = UserModel.query.filter(
+    # (UserModel.location == form.location.data) &
+    # (UserModel.username != form.username.data)
+    # ).all()
+    
+    # if not friends:
+    #     return "No friends found"
+    
+    # return render_template("friends.html", form=form)
 
     
 # @app.route('/', methods=['GET', 'POST'])
