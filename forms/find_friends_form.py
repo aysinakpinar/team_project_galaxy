281d3cdf6b3277c3d569d403524bb41226dc4eac
Amelia Terry <ameliaterry@live.com>
2025-02-28 15:34:39 +0000

    Can search for friends based on age, fitness level + exercise. Friends added to navbar

--- forms/find_friends_form.py
+++ forms/find_friends_form.py
@@ -7,4 +7,6 @@ from wtforms import StringField, SubmitField
 class FindFriendsForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     location = StringField("Location", validators=[DataRequired()])
-    #favourite exercise.
+    age = StringField("Age", validators=[DataRequired()])
+    fitness_level = StringField("Fitness Level", validators=[DataRequired()])
+    favourite_exercise = StringField("Favourite Exercise", validators=[DataRequired()])
