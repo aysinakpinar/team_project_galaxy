323c62f9ad32aa5f3e2d45e09556a61b577f2277
Amelia Terry <ameliaterry@live.com>
2025-03-03 14:05:14 +0000

    User profile minor changes

--- forms/user_profile_form.py
+++ forms/user_profile_form.py
@@ -1,19 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField, FloatField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, EmailField, FloatField, SelectField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, NumberRange, Optional
 from email_validator import *
 
 #Millie duplicated Aysin's code for signup
 class UserProfileForm(FlaskForm):
-    username = StringField("Username", validators=[Optional(), Length(min=3, max=20)])
-    email = EmailField("Email", validators=[Optional(), Email()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[Optional(), Length(min=6)])
     confirm_password = PasswordField("Confirm Password", validators=[Optional(), EqualTo("password")])
-    location = StringField("Location", validators=[Optional()])
+    location = StringField("Location", validators=[DataRequired()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=18, max=150)])
     weight = IntegerField("Weight", validators=[Optional(), NumberRange(min=0)])
     height = FloatField("Height", validators=[Optional(), NumberRange(min=0)])
-    fitness_level = StringField("Fitness Level", validators=[Optional()])
+    fitness_level = SelectField("Fitness Level", 
+                                choices=[
+                                    ("", "Select Fitness Level"),
+                                    ("beginner", "Beginner"),
+                                    ("intermediate", "Intermediate"),
+                                    ("advanced", "Advanced")
+                                ], 
+                                validators=[Optional()])
     favourite_exercise = StringField("Favourite Exercise", validators=[Optional()])
     
 
