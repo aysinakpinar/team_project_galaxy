ce3e18fac5abb5698abc309d44799ac950b8a712
Louis_twt <louistse5283@gmail.com>
2025-03-05 18:00:25 +0000

    fixed creating post feature, created a reply to reply feature. updated html

--- models/reply.py
+++ models/reply.py
@@ -7,9 +7,11 @@ class ReplyModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     post_Id = db.Column(db.Integer, db.ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
     replier_Id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    parent_reply_id = db.Column(db.Integer, db.ForeignKey("replies.id", ondelete="CASCADE"), nullable=True)
     username = db.Column(db.String(200), nullable=False)
     text = db.Column(db.Text, nullable=False)
     img = db.Column(db.String(200), nullable=True)
     replied_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     
+    parent = db.relationship("ReplyModel", remote_side=[id], backref="replies", cascade="all, delete")
 
