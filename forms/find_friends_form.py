e76a1661f91e17bb3cfe6ed36b9139e2dee5dd33
Michal <m.podolak14@gmail.com>
2025-03-06 18:42:04 +0000

    styling(dashboard): update dashboard styling

--- forms/find_friends_form.py
+++ forms/find_friends_form.py
@@ -5,11 +5,11 @@ from wtforms import StringField, SubmitField, HiddenField
 
 
 class FindFriendsForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    location = StringField("Location", validators=[DataRequired()])
-    age = StringField("Age", validators=[DataRequired()])
-    fitness_level = StringField("Fitness Level", validators=[DataRequired()])
-    favourite_exercise = StringField("Favourite Exercise", validators=[DataRequired()])
+    username = StringField("Username")
+    location = StringField("Location")
+    age = StringField("Age")
+    fitness_level = StringField("Fitness Level" )
+    favourite_exercise = StringField("Favourite Exercise")
 
 class AddFriendForm(FlaskForm):
     add_friend = SubmitField("Add friend")
